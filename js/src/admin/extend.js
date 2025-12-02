1b667fb6d3e684b9390d6ef3d328ab227406c39e
Walid <108340184+WalsGit@users.noreply.github.com>
2025-12-02 20:05:18 +0100

    changed:  admin extender

---
+++ js/src/admin/extend.js
@@ -0,0 +1,9 @@
+import Extend from 'flarum/common/extenders';
+import app from 'flarum/admin/app';
+
+import SettingsPage from './components/SettingsPage'
+
+export default [
+    new Extend.Admin()
+        .page(SettingsPage)
+]
--- js/src/admin/index.tsx
+++ js/src/admin/index.tsx
@@ -1,6 +1,8 @@
 import app from 'flarum/admin/app';
 import SettingsPage from './components/SettingsPage';
 
+export { default as extend } from './extend';
+
 app.initializers.add('walsgit/recycle-bin', () => {
   app.registry.for('walsgit-recycle-bin').registerPage(SettingsPage as any);
 });
